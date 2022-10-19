4314648bce4d53096ff19da89ad1b99523513b05
Paulo Filho <pcf.feat@gmail.com>
2022-10-19 09:54:01 -0400

    study: draft logic for animaton presentation

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -21,7 +21,21 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0) {
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        } completion: { finished in
+            sender.isHidden = false
+        }
+
+        
         currentAnimation += 1
         if currentAnimation > 7 {
             currentAnimation = 0
