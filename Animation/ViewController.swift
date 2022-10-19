562e93ba49aead8720810fb60d7e2436895b1d7e
Paulo Filho <pcf.feat@gmail.com>
2022-10-19 09:13:29 -0400

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Paulo Filho on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
